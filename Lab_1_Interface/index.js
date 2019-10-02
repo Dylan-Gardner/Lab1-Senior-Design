04d01cc429b6e5880ce4e9459e29c383df488419
Nick Grove <nicholas-grove@uiowa.edu>
2019-10-02 18:05:07 -0500

    Front End finished

--- Lab_1_Interface/index.js
+++ Lab_1_Interface/index.js
@@ -1,20 +1,21 @@
 // Page Load
 window.onload = function() {
-    data = makeData();
-    drawGraph(data);
-    document.getElementById("current-temp-visual").innerHTML = Number.parseFloat(data[data.length-1].temp).toFixed(1);
+    // dataset = makeData();
+    csvData();
+    drawGraph(dataset);
+    //updateGraph(dataset);
 };
 
 // Update Data
 function update() {
     generate();
     if (currentunit == "°C"){
-        document.getElementById("current-temp-visual").innerHTML = Number.parseFloat(data[data.length-1].temp).toFixed(1);
+        document.getElementById("current-temp-visual").innerHTML = Number.parseFloat(dataset[dataset.length-1].temp).toFixed(1);
     } else {
-        var tempNum = (data[data.length-1].temp * 9/5) + 32
+        var tempNum = (dataset[dataset.length-1].temp * 9/5) + 32
         document.getElementById("current-temp-visual").innerHTML = Number.parseFloat(tempNum).toFixed(1);
     }
-    updateGraph(data);
+    updateGraph(dataset);
 }
 
 // Generate Data for Demo
@@ -26,18 +27,18 @@ function generate() {
     //     });
     // } else {
 
-    data.push({
+    dataset.push({
         time: 0,
         temp: +Math.random() * (30.0 - 20.0) + 20.0
     });
 
     // }
 
-    for (i = 0; i < data.length-1; i++) {
-        data[i].time = data[i].time + 1;
+    for (i = 0; i < dataset.length-1; i++) {
+        dataset[i].time = dataset[i].time + 1;
     }
-    if(data.length >= shiftpoint) {
-        data.shift();
+    if(dataset.length >= shiftpoint) {
+        dataset.shift();
     }
     
 
@@ -49,64 +50,119 @@ function makeData() {
 
     arr = [];
     arr.push({
+        time: 5, 
+        temp: +Math.random() * (30.0 - 20.0) + 20.0
+    })
+    arr.push({
+        time: 4, 
+        temp: +Math.random() * (30.0 - 20.0) + 20.0
+    })
+    arr.push({
+        time: 3, 
+        temp: +Math.random() * (30.0 - 20.0) + 20.0
+    })
+    arr.push({
+        time: 2, 
+        temp: +Math.random() * (30.0 - 20.0) + 20.0
+    })
+    arr.push({
+        time: 1, 
+        temp: +Math.random() * (30.0 - 20.0) + 20.0
+    })
+    arr.push({
         time: 0, 
         temp: +Math.random() * (30.0 - 20.0) + 20.0
-    });
+    })
     return arr;
 
 }
 
 // Draw The Graph, No Data
-function drawGraph(data) {
+// function drawGraph() {
 
-var svgWidth = 800, svgHeight = 400;
-var margin = { top: 20, right: 20, bottom: 30, left: 50 };
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
+//     var svgWidth = 800, svgHeight = 400;
+//     var margin = { top: 20, right: 20, bottom: 30, left: 50 };
+//     var width = svgWidth - margin.left - margin.right;
+//     var height = svgHeight - margin.top - margin.bottom;
 
-
-var svg = d3.select('#line-chart')
-    .attr("width", svgWidth)
-    .attr("height", svgHeight)
+    // var svg = d3.select('#line-chart')
+    //     .attr("width", svgWidth)
+    //     .attr("height", svgHeight)
     
-var g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    // var g = svg.append("g")
+    //     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var x = d3.scaleTime()
-    .domain([300, 0])
-    .rangeRound([0, width]);
+    // var x = d3.scaleTime()
+    //     .domain([300, 0])
+    //     .rangeRound([0, width]);
 
-var y = d3.scaleLinear()
-    .domain([min, max])
-    .rangeRound([height, 0]);  
+    // var y = d3.scaleLinear()
+    //     .domain([min, max])
+    //     .rangeRound([height, 0]);  
 
-var line = d3.line()
-    .x(function(d) { return x(d.time)})
-    .y(function(d) { return y(d.temp)});   
+    // var line = d3.line()
+    //     .x(function(d) { return x(d.time)})
+    //     .y(function(d) { return y(d.temp)});   
 
-g.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x)
-    .tickFormat(d3.format(".0f")))
-    .append("text")
-    .attr("fill", "#000")
-    .attr("y", -10)
-    .attr("dx", "70.0em")
-    .text("Seconds Ago");
+    // g.append("g")
+    //     .attr("transform", "translate(0," + height + ")")
+    //     .call(d3.axisBottom(x)
+    //     .tickFormat(d3.format(".0f")))
+    //     .append("text")
+//         .attr("fill", "#000")
+//         .attr("y", -10)
+//         .attr("dx", "70.0em")
+//         .text("Seconds Ago");
 
-g.append("g")
-    .call(d3.axisLeft(y))
-    .append("text")
-    .attr("fill", "#000")
-    .attr("x", 80)
-    .attr("dy", "1.0em")
-    .text("Temperature (°C)");
+//     g.append("g")
+//         .call(d3.axisLeft(y))
+//         .append("text")
+//         .attr("fill", "#000")
+//         .attr("x", 80)
+//         .attr("dy", "1.0em")
+//         .text("Temperature (°C)");    
+
+//     d3.csv("temp_data.csv")
+//         .get(function(error,data){  
+
+//         tempData = data;
+//         storeData(tempData);    
+
+//         g.append("path")
+//             .datum(dataset)
+//             .attr("fill", "none")
+//             .attr("stroke", "rgb(0, 147, 175)")
+//             .attr("stroke-width", 2.0)
+//             .attr("d", line);  
+        
+//         document.getElementById("current-temp-visual").innerHTML = Number.parseFloat(dataset[dataset.length-1].temp).toFixed(1);     
+        
+//     });    
+
+// }
+
+function csvData() {
+
+    d3.csv("temp_data.csv")
+    .get(function(error,data){  
+
+        var tempData = data;
+        storeData(tempData);
+
+    });
 
 }
 
-// Update The Graph
-function updateGraph(data) {
+function storeData(tD) {
+    tD.forEach(function(d){
+        dataset.push({
+            time: +d.time,
+            temp: +d.temp
+        })    
+    })
+}
 
+function drawGraph(dataset) {
     var svgWidth = 800, svgHeight = 400;
     var margin = { top: 20, right: 20, bottom: 30, left: 50 };
     var width = svgWidth - margin.left - margin.right;
@@ -114,21 +170,10 @@ function updateGraph(data) {
     
     var svg = d3.select('#line-chart')
         .attr("width", svgWidth)
-        .attr("height", svgHeight)
-
-    var u = d3.select('#line-chart')
-        .selectAll('path')
-        .data(data)
-        
-    u.exit().remove();    
-
-    // var yAx = d3.selectAll('.ticks')
-
-    // yAx.exit().remove();
+        .attr("height", svgHeight)      
         
     var g = svg.append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
     
     var x = d3.scaleTime()
         .domain([300, 0])
@@ -159,22 +204,124 @@ function updateGraph(data) {
         .attr("fill", "#000")
         .attr("x", 80)
         .attr("dy", "1.0em")
-        .text("Temperature (°C)");
+        .text("Temperature (°C)");  
+
+    // var path = svg.selectAll("path").data(dataset);
+
+    // path.exit().remove();
+
+    // g.append("path")
+    //     .datum(dataset)
+    //     .attr("fill", "none")
+    //     .attr("stroke", "rgb(0, 147, 175)")
+    //     .attr("stroke-width", 2.0)
+    //     .attr("d", line);    
+
+    // path.enter().append("path")   
+    
+    // console.log("initial draw")
+    
+}
+
+// Update The Graph
+function updateGraph(dataset) {
+
+    var svgWidth = 800, svgHeight = 400;
+    var margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    var width = svgWidth - margin.left - margin.right;
+    var height = svgHeight - margin.top - margin.bottom;
+
+    d3.selectAll("svg > *").remove();
+    
+    var svg = d3.select('#line-chart')
+        .attr("width", svgWidth)
+        .attr("height", svgHeight)       
+        
+    var g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")"); 
+        
+    var path = svg.selectAll("path").data(dataset);
+    
+    var x = d3.scaleTime()
+        .domain([300, 0])
+        .rangeRound([0, width]);
+    
+    var y = d3.scaleLinear()
+        .domain([min, max])
+        .rangeRound([height, 0]);  
+    
+    var line = d3.line()
+        .defined(function (d){ return d.temp != 0; })
+        .x(function(d) { return x(d.time)})
+        .y(function(d) { return y(d.temp)});
+    
+    g.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x)
+        .tickFormat(d3.format(".0f")))
+        .append("text")
+        .attr("fill", "#000")
+        .attr("y", -10)
+        .attr("dx", "70.0em")
+        .text("Seconds Ago");
+    
+    g.append("g")
+        .call(d3.axisLeft(y))
+        .append("text")
+        .attr("fill", "#000")
+        .attr("x", 80)
+        .attr("dy", "1.0em")
+        .text("Temperature (°C)");  
 
     g.append("path")
-        .datum(data)
+        .datum(dataset)
         .attr("fill", "none")
         .attr("stroke", "rgb(0, 147, 175)")
         .attr("stroke-width", 2.0)
-        .attr("d", line);   
+        .attr("d", line);
+
+    //path.exit().remove();    
+
+    //path.enter().append("path")     
+
+    // else {
+
+    //     var path = svg.selectAll("path").data(dataset);
+
+    //     path.enter().append("path")
+
+    // }
+
+    // d3.select('#line-chart')
+    //     .selectAll('path')
+    //     .data(dataset)
+    //     .enter();    
+        
+    //document.getElementById("current-temp-visual").innerHTML = Number.parseFloat(dataset[dataset.length-1].temp).toFixed(1);  
+
+        // g.append("path")
+        //     .datum(dataset)
+        //     .attr("fill", "none")
+        //     .attr("stroke", "rgb(0, 147, 175)")
+        //     .attr("stroke-width", 2.0)
+        //     .attr("d", line)    
+ 
 
 }
 
-document.getElementById('unit-change-button').onclick = function changeContent() {
+document.getElementById('unit-change-button').onclick = function changeUnits() {
     if (document.getElementById('unit-change-button').innerHTML == "Change to Fahrenheit"){
         unitChange("°F", "Celsius");
     } else if (document.getElementById('unit-change-button').innerHTML == "Change to Celsius") {
         unitChange("°C", "Fahrenheit");
+    }
+}
+
+document.getElementById('text-update-entry').onkeypress = function(e){
+    if (!e) e = window.event;
+    var keyCode = e.keyCode || e.which;
+    if (keyCode == '13'){
+        alert("Setting up Text Updates with " + document.getElementById('text-update-entry').value);
     }
 }
 
@@ -211,10 +358,11 @@ function unitChange(unit, string) {
     document.getElementById('current-temp-heading').innerHTML = "Current Temperature (" + unit + ")";
     currentunit = unit;
     update();
-    updateGraph(data);
+    updateGraph();
 }
 
-var data = [];
+var init = true;
+var dataset = [];
 var refesh = setInterval(update, 1000);
 var shiftpoint = 301;
 var max = 50;
